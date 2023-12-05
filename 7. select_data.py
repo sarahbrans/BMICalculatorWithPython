edb946dcf6cefffe4177dde4bdeef9eb903630c6
Sarah Branscum <119828032+sarahbrans@users.noreply.github.com>
2023-12-05 16:14:04 -0600

    Delete 7. select_data.py

--- 7. select_data.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost", user="root", password="Heyroot2001!", database="bmicalc")
-mycursor=mydb.cursor()
-
-mycursor.execute("SELECT * FROM bmi_record")
-result= mycursor.fetchall()
-for row in result:
-    print(row)
