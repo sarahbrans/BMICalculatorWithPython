251a28cc3e5142871a3f373c290adf62388c8815
Sarah Branscum <119828032+sarahbrans@users.noreply.github.com>
2023-12-05 18:58:55 -0600

    Delete BMI Calculator directory

--- BMI Calculator/4. show_databases.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost", user="root", password="Heyroot2001!")
-mycursor=mydb.cursor()
-
-mycursor.execute("SHOW DATABASES")
-for db in mycursor:
-    print(db)
