d702696ba0960bc6b8afcd688cc88d5424725990
Sarah Branscum <119828032+sarahbrans@users.noreply.github.com>
2023-12-05 19:02:40 -0600

    Update 10. delete_data.py

--- PythonMySQLConnection/10. delete_data.py
+++ PythonMySQLConnection/10. delete_data.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost", user="root", password="Heyroot2001!", database="bmicalc")
+mydb=mysql.connector.connect(host="localhost", user="root", password="******", database="bmicalc")
 mycursor=mydb.cursor()
 
 mycursor.execute("DELETE bmi_record WHERE ID = '20'")
